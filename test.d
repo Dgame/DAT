4fd4c08674ea891b0a0e4c8a9bd0617d8b3cf173
Dgame <rswhite4@googlemail.com>
2013-11-26 21:24:49 +0100

    Reduced test example.

--- test.d
+++ test.d
@@ -97,140 +97,30 @@ void FR_test() {
 	int abc;
 }
 
-struct Heap {
+struct A {
 	private import core.stdc.stdlib : malloc, realloc, free;
-	private import core.stdc.string : memset;
-	
-	T[] allocate(T = void)(size_t bytes) {
-		const size_t cap = bytes * T.sizeof;
-		
-		void* p = malloc(cap);
-		debug writefln(" -> Allocate (ptr = %x) %d bytes on heap.", p, cap);
-		
-		if (p !is null) {
-			memset(p, 0, cap);
-			
-			static if (is(T == void))
-				return p[0 .. bytes];
-			else
-				return (cast(T*) p)[0 .. bytes];
-		}
-		
-		return null;
+
+	void* allocate(size_t bytes) {
+		return malloc(bytes);
 	}
-	
-	void reallocate(T = void)(ref T[] arr, size_t bytes) {
-		const size_t cap = arr.length + (bytes * T.sizeof);
-		
-		debug writefln(" -> Reallocate (ptr = %x) %d bytes on heap.", arr.ptr, cap);
-		
-		if (cap == 0 || cap >= int.max) {
-			this.deallocate(arr);
-			arr = null;
-			
-			return;
-		}
-		
-		void* p = realloc(arr.ptr, cap);
-		if (p !is null) {
-			static if (is(T == void))
-				arr = p[0 .. bytes];
-			else
-				arr = (cast(T*) p)[0 .. bytes];
-		}
+
+	void reallocate(void* ptr, size_t bytes) {
+		ptr = realloc(ptr, bytes);
 	}
-	
-	void deallocate(T = void)(ref T[] arr) {
-		debug writefln(" -> Free (ptr = %x) %d bytes from heap.", arr.ptr, arr.length);
-		free(arr.ptr);
-		arr = null;
+
+	void deallocate(void* ptr) {
+		free(ptr);
 	}
 }
 
-struct Stack {
-	void[4096] _buffer = void;
-	size_t _length = 0;
-	
-	void[] take(size_t bytes) {
-		if (this._length + bytes > this._buffer.length)
-			return null;
-		
-		const size_t index = this._length;
-		this._length += bytes;
-		
-		return this._buffer[index .. this._length];
-	}
-	
-	size_t remain() const pure nothrow {
-		return this._buffer.length - this._length;
-	}
-}
-
-struct TempAlloc {
+struct B {
 	private import core.stdc.stdlib : free;
-	
-	Heap _heap;
-	Stack _stack;
-	
-	// TODO: improve
-	void*[32] _ptrs = void;
-	size_t _length = 0;
-	
-	@disable
-	this(this);
-	
-	~this() {
-		this.freeAll();
+
+	void*[128] _ptrs;
+
+	void freeAll() {
+		for (size_t i = 0; i < this._ptrs.length; i++) {
+			free(this._ptrs[i]);
+		}
 	}
-	
-	//private void _store(void* ptr) {
-	//	if (this._length == this._ptrs.length) {
-	//		free(ptr);
-	//		this.freeAll();
-			
-	//		assert(0, "Out of memory.");
-	//	}
-		
-	//	this._ptrs[this._length++] = ptr;
-	//}
-	
-	//void freeAll() {
-	//	debug writefln(" --> Free All (%d)", this._length);
-		
-	//	for (int i = cast(int)(this._length - 1); i >= 0; i--) {
-	//		debug writefln(" -> Free (ptr = %x).", this._ptrs[i]);
-	//		free(this._ptrs[i]);
-	//	}
-	//}
-	
-	//T[] allocate(T = void)(size_t bytes) {
-	//	static if (is(T == void))
-	//		void[] result = this._stack.take(bytes);
-	//	else
-	//		T[] result = cast(T[]) this._stack.take(bytes * T.sizeof);
-		
-	//	if (result.length != 0) {
-	//		debug writefln(" --> Allocate %d bytes on the stack", bytes * T.sizeof);
-			
-	//		return result;
-	//	}
-		
-	//	result = this._heap.allocate!T(bytes);
-	//	if (result.length != 0)
-	//		this._store(result.ptr);
-		
-	//	return result;
-	//}
-	
-	//void reallocate(T = void)(ref T[] arr, size_t bytes) {
-	//	void* pc = arr.ptr;
-	//	this._heap.reallocate(arr, bytes);
-		
-	//	if (pc !is arr.ptr)
-	//		this._store(arr.ptr);
-	//}
-	
-	//void deallocate(T = void)(ref T[] arr) {
-	//	this._heap.deallocate(arr);
-	//}
 }
